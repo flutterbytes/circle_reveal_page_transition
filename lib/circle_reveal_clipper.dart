53b339fd5059a519ea84e92ce1e11a2ef17cddef
Code Mode <dragonappslab@gmail.com>
2020-12-05 19:28:05 +0545

    Push

--- lib/circle_reveal_clipper.dart
+++ lib/circle_reveal_clipper.dart
@@ -2,16 +2,13 @@ import 'package:flutter/material.dart';
 
 class CircleRevealClipper extends CustomClipper<Path> {
   final center;
-  final animationValue;
   final radius;
 
-  CircleRevealClipper({this.center, this.animationValue, this.radius});
+  CircleRevealClipper({this.center, this.radius});
 
   @override
   Path getClip(Size size) {
-    return Path()
-      ..addOval(
-          Rect.fromCircle(radius: radius * animationValue, center: center));
+    return Path()..addOval(Rect.fromCircle(radius: radius, center: center));
   }
 
   @override
--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:transition_animations/circle_reveal_clipper.dart';
 import 'package:transition_animations/second_page.dart';
@@ -12,79 +10,32 @@ class FirstPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("First Page"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //we have a basic navigator
-            //push function
-            Navigator.of(context).push(_createRoute());
-          },
-          child: Text("Second Page"),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(context, _createRoute()),
       ),
+      body: Center(),
     );
   }
 
   Route _createRoute() {
     return PageRouteBuilder(
-      //these are arguments
-      //pagebuilder, transitionbuilder, transitionduration and reversetransitionduration
-
       transitionDuration: Duration(milliseconds: 2000),
       reverseTransitionDuration: Duration(milliseconds: 2000),
       opaque: false,
       barrierDismissible: false,
-
       pageBuilder: (context, animation, secondaryAnimation) => SecondPage(),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        //first
-        FadeTransition(
-          opacity: animation,
-          child: child,
-        );
+        var screenSize = MediaQuery.of(context).size;
+        Offset center = Offset(screenSize.width - 40, screenSize.height - 40);
+        double beginRadius = 0.0;
+        double endRadius = screenSize.height * 1.2;
 
-        //second
-        double beginScale = 0.4;
-        double endScale = 0.50;
+        var tween = Tween(begin: beginRadius, end: endRadius);
+        var radiusTweenAnimation = animation.drive(tween);
 
-        var scaleTween = Tween(begin: beginScale, end: endScale);
-        var scaleTweenAnimation = animation.drive(scaleTween);
-        print(scaleTweenAnimation.value);
-        return ScaleTransition(
-          scale: scaleTweenAnimation,
-          child: child,
-        );
-
-        //third
-        // zero on the x axis is left and 1 is right
-        // zero on the y is top and 1 is bottom
-
-        Offset beginOffset = Offset(0, 1);
-        Offset endOffset = Offset(0, 0);
-
-        var tween = Tween(begin: beginOffset, end: endOffset);
-        var offsetTweenAnimation = animation.drive(tween);
-
-        SlideTransition(
-          position: offsetTweenAnimation,
-          child: child,
-        );
-
-        //fourth
-        Transform(
-          transform: Matrix4.identity()..rotateY(animation.value * pi / 2),
-          child: child,
-        );
-
-        //fifth
-        var size = MediaQuery.of(context).size;
-        Offset startingPoint = Offset(size.width, size.height);
-
-        ClipPath(
-          clipper: CircleRevealClipper(
-              animationValue: animation.value,
-              radius: size.height * 1.2,
-              center: startingPoint),
+        return ClipPath(
+          clipper:
+              CircleRevealClipper(radius: radiusTweenAnimation, center: center),
           child: child,
         );
       },
